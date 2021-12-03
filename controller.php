ef3848935848c3ac8607a34baef75bee3ba8dbe5
Dave <david@afixia.com>
2021-12-03 12:24:14 -0600

    made my own contentSwap to fix install

--- controller.php
+++ controller.php
@@ -9,9 +9,10 @@ use Concrete\Core\Package\Package;
 use Concrete\Core\Page\Page;
 use Concrete\Core\Page\PageList;
 use Concrete\Core\Page\Stack\StackList;
-use Concrete\Core\Page\Theme\Theme;
-use Concrete\Core\File\Image\Thumbnail\Type\Type;
-use Concrete\Core\Entity\File\Image\Thumbnail\Type\Type as ThumbnailType;
+use Concrete\Core\File\StorageLocation\StorageLocationFactory;
+use Concrete\Core\Page\Type\Type;
+use Concrete\Core\User\User;
+use Concrete\Core\Http\Request;
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
@@ -23,7 +24,7 @@ class Controller extends Package {
     protected $themeHandle = 'atomik_clone'; //Change Theme Handle
     protected $appVersionRequired = '9.0.0';
     protected $pkgVersion = '1.0';
-    protected $pkgAllowsFullContentSwap = true;
+//    protected $pkgAllowsFullContentSwap = true; // This doesn't really work here.
     protected $pkgContentProvidesFileThumbnails = true;
 
     //Replace ThemeName with the name of the theme
@@ -45,151 +46,71 @@ class Controller extends Package {
 
     public function install() {
         $pkg = parent::install();
-        Theme::add($this->themeHandle, $pkg);
 
         $this->install_config();
         $this->install_themes();
 
-//        $xs = Type::getByHandle('xs');
-//        if (!is_object($xs)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Extra Small');
-//            $type->setHandle('xs');
-//            $type->setWidth(540);
-//            $type->save();
-//        }
-//
-//        $sm = Type::getByHandle('sm');
-//        if (!is_object($sm)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Small');
-//            $type->setHandle('sm');
-//            $type->setWidth(540);
-//            $type->save();
-//        }
-//
-//        $md = Type::getByHandle('md');
-//        if (!is_object($md)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Medium');
-//            $type->setHandle('md');
-//            $type->setWidth(720);
-//            $type->save();
-//        }
-//
-//        $lg = Type::getByHandle('lg');
-//        if (!is_object($lg)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Large');
-//            $type->setHandle('lg');
-//            $type->setWidth(960);
-//            $type->save();
-//        }
-//
-//        $xl = Type::getByHandle('xl');
-//        if (!is_object($xl)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Extra Large');
-//            $type->setHandle('xl');
-//            $type->setWidth(1140);
-//            $type->save();
-//        }
-
-//        $resource_list_entry = Type::getByHandle('resource_list_entry');
-//        if (!is_object($resource_list_entry)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Resource List Entry');
-//            $type->setHandle('resource_list_entry');
-//            $type->setWidth(510);
-//            $type->setHeight(510);
-//            $type->setSizingMode($type::RESIZE_PROPORTIONAL);
-//            $type->save();
-//        }
-//
-//        $testimonial_circle = Type::getByHandle('testimonial_circle');
-//        if (!is_object($testimonial_circle)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Testimonial Circle');
-//            $type->setHandle('testimonial_circle');
-//            $type->setWidth(180);
-//            $type->setHeight(180);
-//            $type->setSizingMode($type::RESIZE_EXACT);
-//            $type->save();
-//        }
-//
-//        $stripe_column = Type::getByHandle('stripe_column');
-//        if (!is_object($stripe_column)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Stripe Column Image');
-//            $type->setHandle('stripe_column');
-//            $type->setWidth(850);
-//            $type->setHeight(650);
-//            $type->setSizingMode($type::RESIZE_EXACT);
-//            $type->save();
-//        }
-//
-//        $atomik_gallery = Type::getByHandle('atomik_gallery');
-//        if (!is_object($atomik_gallery)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Gallery');
-//            $type->setHandle('atomik_gallery');
-//            $type->setWidth(860);
-//            $type->setHeight(614);
-//            $type->setSizingMode($type::RESIZE_EXACT);
-//            $type->save();
-//        }
-//
-//        $blog_entry_thumbnail = Type::getByHandle('blog_entry_thumbnail');
-//        if (!is_object($blog_entry_thumbnail)) {
-//            $type = new ThumbnailType();
-//            $type->setName('Blog Entry Thumbnail');
-//            $type->setHandle('blog_entry_thumbnail');
-//            $type->setWidth(660);
-//            $type->setHeight(428);
-//            $type->setSizingMode($type::RESIZE_EXACT);
-//            $type->save();
-//        }
+        $r = Request::getInstance();
+        $r->request->all();
+        if ($r->request->get('pkgDoFullContentSwap')) {
+            // install sample content.
+            $this->swapContent($r->request->all());
+        }
     }
 
     public function swapContent($options)
     {
-        if ($this->validateClearSiteContents($options)) {
-            $this->app->make('cache/request')->disable();
+        //pkgDoFullContentSwap
+        $u = $this->app->make(User::class);
+        if ($u->isSuperUser()) {
+            // this can ONLY be used through the post. We will use the token to ensure that
+            $valt = $this->app->make('helper/validation/token');
+            if ($valt->validate('install_options_selected', $options['ccm_token'])) {
+                $this->app->make('cache/request')->disable();
 
-            $pl = new PageList();
-            $pages = $pl->getResults();
-            foreach ($pages as $c) {
-                $c->delete();
+                $pl = new PageList();
+                $pages = $pl->getResults();
+                foreach ($pages as $c) {
+                    $c->delete();
+                }
+
+                $fl = new FileList();
+                $files = $fl->getResults();
+                foreach ($files as $f) {
+                    $f->delete();
+                }
+
+                // clear stacks
+                $sl = new StackList();
+                foreach ($sl->getResults() as $c) {
+                    $c->delete();
+                }
+
+                $home = \Page::getByID(\Page::getHomePageID());
+                $blocks = $home->getBlocks();
+                foreach ($blocks as $b) {
+                    $b->deleteBlock();
+                }
+
+                $pageTypes = Type::getList();
+                foreach ($pageTypes as $ct) {
+                    $ct->delete();
+                }
+
+                // Set the page type of the home page to 0, because
+                // if it has a type the type will be gone since we just
+                // deleted it
+                $home = Page::getByID(\Page::getHomePageID());
+                $home->setPageType(null);
+
+//                $this->writeLog('content swap 2');
+                $this->install_file_manager();
+                $this->import_files();
+                $this->move_files();
+                $this->install_content();
+
+                $this->app->make('cache/request')->enable();
             }
-
-            $fl = new FileList();
-            $files = $fl->getResults();
-            foreach ($files as $f) {
-                $f->delete();
-            }
-
-            $sl = new StackList();
-            foreach ($sl->getResults() as $c) {
-                $c->delete();
-            }
-
-            $home = Page::getByID(1);
-            $blocks = $home->getBlocks();
-            foreach ($blocks as $b) {
-                $b->deleteBlock();
-            }
-
-            $pageTypes = PageType::getList();
-            foreach ($pageTypes as $ct) {
-                $ct->delete();
-            }
-
-            $this->install_file_manager();
-            $this->import_files();
-            $this->move_files();
-            $this->install_content();
-
-            $this->app->make('cache/request')->enable();
         }
     }
 
@@ -213,18 +134,20 @@ class Controller extends Package {
 
     public function install_file_manager()
     {
+        $fsl = $this->app->make(StorageLocationFactory::class)->fetchDefault();
+
         // Create documents node in file manager
         $filesystem = new Filesystem();
         $root = $filesystem->getRootFolder();
-        $documents = $filesystem->addFolder($root, 'Documents');
-        $brand = $filesystem->addFolder($root, 'Brand');
-        $blog = $filesystem->addFolder($root, 'Blog');
-        $gallery = $filesystem->addFolder($root, 'Gallery');
-        $collaboration = $filesystem->addFolder($root, 'Collaboration Slider');
-        $heroes = $filesystem->addFolder($root, 'Hero Images');
-        $logoSlider = $filesystem->addFolder($root, 'Logo Slider');
-        $stripes = $filesystem->addFolder($root, 'Stripes');
-        $team = $filesystem->addFolder($root, 'Team');
+        $documents = $filesystem->addFolder($root, 'Documents', $fsl->getID());
+        $brand = $filesystem->addFolder($root, 'Brand', $fsl->getID());
+        $blog = $filesystem->addFolder($root, 'Blog', $fsl->getID());
+        $gallery = $filesystem->addFolder($root, 'Gallery', $fsl->getID());
+        $collaboration = $filesystem->addFolder($root, 'Collaboration Slider', $fsl->getID());
+        $heroes = $filesystem->addFolder($root, 'Hero Images', $fsl->getID());
+        $logoSlider = $filesystem->addFolder($root, 'Logo Slider', $fsl->getID());
+        $stripes = $filesystem->addFolder($root, 'Stripes', $fsl->getID());
+        $team = $filesystem->addFolder($root, 'Team', $fsl->getID());
     }
 
     public function import_files()
--- elements/dashboard/install.php
+++ elements/dashboard/install.php
@@ -1,5 +1,41 @@
-<?php  defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
+<?php
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+use Concrete\Core\Support\Facade\Application;
+$app = Application::getFacadeApplication();
+$form = $app->make('helper/form');
+$u = $app->make(Concrete\Core\User\User::class);
+?>
 
 <div class="alert alert-danger">
     <?php echo t('<strong>Attention!</strong> Clearing your site\'s content prior to installing this theme is highly recommended.')?>
+</div>
+
+<h4><?=t('Clear this Site?')?></h4>
+<p><?=t('Atomik Clone can fully clear your website of all existing content and install its own custom content in its place. If you\'re installing a theme for the first time you may want to do this. Clear all site content?') ?></p>
+<?php
+
+if ($u->isSuperUser()) {
+    $disabled = [];
+    ?>
+    <div class="alert-message warning"><p><?=t('This will clear your home page, uploaded files and any content pages out of your site completely. It will completely reset your site and any content you have added will be lost.')?></p></div>
+    <?php
+} else {
+    $disabled = ['disabled'=>true];
+    ?>
+    <div class="alert-message info"><p><?=t('Only the %s user may reset the site\'s content.', USER_SUPER)?></p></div>
+    <?php
+}
+?>
+<div class="form-group">
+    <label class="control-label form-label"><?=t("Swap Site Contents")?></label>
+    <div class="form-check">
+        <?=$form->radio('pkgDoFullContentSwap',0, true, $disabled)?>
+        <?=$form->label('pkgDoFullContentSwap1',t('No. Do <strong>not</strong> remove any content or files from this website.'))?>
+    </div>
+    <div class="form-check">
+        <?=$form->radio('pkgDoFullContentSwap',1, false, $disabled)?>
+        <?=$form->label('pkgDoFullContentSwap2',t('Yes. Reset site content with the content found in this package'))?>
+    </div>
+
 </div>
