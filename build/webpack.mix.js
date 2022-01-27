baa0c8f124d4336effdbc66e6b99be8c18533fcd
Dave <david@afixia.com>
2022-01-27 12:25:10 -0600

    updated to mirror atomik from 9.0.2

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -16,6 +16,7 @@ mix.webpackConfig({
 
 mix
     .sass('assets/scss/presets/default/main.scss', 'css/skins/default.css')
+    // Comment this next line out if you don't need an extra color option.  It just adds to the compile time.
     .sass('assets/scss/presets/rustic-elegance/main.scss', 'css/skins/rustic-elegance.css')
     .js('assets/js/main.js', '').vue();
 
@@ -23,4 +24,8 @@ mix.options({
     processCssUrls: false
 });
 
-mix.setPublicPath('../themes/atomik_clone');
+mix.setPublicPath('../themes/atomik_clone'); // Change this if you change your theme naming / file structure.
+
+mix.browserSync({
+    proxy: 'atomik-clone.test' // You need to change this to your local dev URL for npm run watch or npx mix watch
+});
