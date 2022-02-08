ff49b45f7c0a1d3e5c8dc02b834cdcb72efa91e4
Dave <david@afixia.com>
2022-02-08 12:21:57 -0600

    simplified package.json and webpack.mix.js

--- build/webpack.mix.js
+++ build/webpack.mix.js
@@ -1,47 +1,20 @@
-let mix = require('laravel-mix');
+let mix = require("laravel-mix");
 
 mix.webpackConfig({
-    resolve: {
-        symlinks: false
-    },
-    externals: {
-        jquery: 'jQuery',
-        bootstrap: true,
-        vue: 'Vue',
-        moment: 'moment'
-    },
-    module: {
-        rules: [
-            {
-                resolve: {
-                    alias: {
-                        "concretecms-bedrock": "@concretecms/bedrock"
-                    }
-                }
-            }
-        ],
-    },
-})
-mix.options({
-    processCssUrls: false
-})
-    .setPublicPath('../themes/atomik_clone') // Change this if you change your theme naming / file structure.
-    .sass('assets/scss/presets/default/main.scss', 'css/skins/default.css', {
-        sassOptions: {
-            includePaths: [
-                path.resolve(__dirname, './node_modules/')
-            ]
-        }
-    })
-    // Comment this next sass definition out if you don't need an extra color option.  It just adds to the compile time.
-    .sass('assets/scss/presets/rustic-elegance/main.scss', 'css/skins/rustic-elegance.css', {
-        sassOptions: {
-            includePaths: [
-                path.resolve(__dirname, './node_modules/')
-            ]
-        }
-    })
-    .js('assets/js/main.js', '')
-    .browserSync({
-        proxy: 'c59.test' // You need to change this to your local dev URL for npm run watch or npx mix watch
-    });
+  externals: {
+    jquery: "jQuery",
+    bootstrap: true,
+    vue: "Vue",
+    moment: "moment",
+  }
+});
+mix
+  .setPublicPath("../themes/atomik_clone") // Change this if you change your theme naming / file structure.
+  .sass("assets/scss/presets/default/main.scss", "css/skins/default.css")
+  // Comment this next sass definition out if you don't need an extra color option.  It just adds to the compile time.
+  .sass("assets/scss/presets/rustic-elegance/main.scss", "css/skins/rustic-elegance.css")
+  .js("assets/js/main.js", "").vue();
+
+// mix.browserSync({
+//     proxy: 'c59.test' // You need to change this to your local dev URL for npm run watch or npx mix watch
+// });
